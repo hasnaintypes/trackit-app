401f567c1bf4186c32a5c8f11f896ca8e67d89ca
hasnaintypes <hasnainoffice2024@gmail.com>
2025-10-26 00:01:00 -0700

    feat(ui): Add core shadcn-ui components
    This includes Button, Input, Card, and other foundational components.
    Configures component structure and tailwind utility classes for ui theming.

---
+++ src/components/ui/logo.tsx
@@ -0,0 +1,243 @@
+import { cn } from "@/lib/utils";
+
+export const LogoIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      width={74}
+      height={100}
+      viewBox="0 0 74 100"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={cn("h-6 w-auto", className)}
+    >
+      <mask
+        id="mask0_1:52"
+        style={{ maskType: "alpha" as const }}
+        maskUnits="userSpaceOnUse"
+        x="1"
+        y="1"
+        width="71"
+        height="98"
+      >
+        <path
+          d="M45.398 1.43036H7.86688C4.22415 1.43036 1.24374 4.41077 1.24374 8.0535V91.9465C1.24374 95.5893 4.22415 98.5697 7.86688 98.5697H65.2674C68.9101 98.5697 71.8905 95.5893 71.8905 91.9465V27.9229L45.398 1.43036Z"
+          fill="white"
+        />
+      </mask>
+      <g mask="url(#mask0_1:52)">
+        <path
+          d="M45.398 1.43036H7.86688C4.22415 1.43036 1.24374 4.41077 1.24374 8.0535V91.9465C1.24374 95.5893 4.22415 98.5697 7.86688 98.5697H65.2674C68.9101 98.5697 71.8905 95.5893 71.8905 91.9465V27.9229L56.4365 16.8843L45.398 1.43036Z"
+          fill="#0F9D58"
+        />
+      </g>
+      <mask
+        id="mask1_1:52"
+        style={{ maskType: "alpha" as const }}
+        maskUnits="userSpaceOnUse"
+        x="1"
+        y="1"
+        width="71"
+        height="98"
+      >
+        <path
+          d="M45.398 1.43036H7.86688C4.22415 1.43036 1.24374 4.41077 1.24374 8.0535V91.9465C1.24374 95.5893 4.22415 98.5697 7.86688 98.5697H65.2674C68.9101 98.5697 71.8905 95.5893 71.8905 91.9465V27.9229L45.398 1.43036Z"
+          fill="white"
+        />
+      </mask>
+      <g mask="url(#mask1_1:52)">
+        <path
+          d="M18.9054 48.8962V80.908H54.2288V48.8962H18.9054ZM34.3594 76.4926H23.3209V70.9733H34.3594V76.4926ZM34.3594 67.6617H23.3209V62.1424H34.3594V67.6617ZM34.3594 58.8309H23.3209V53.3116H34.3594V58.8309ZM49.8134 76.4926H38.7748V70.9733H49.8134V76.4926ZM49.8134 67.6617H38.7748V62.1424H49.8134V67.6617ZM49.8134 58.8309H38.7748V53.3116H49.8134V58.8309Z"
+          fill="#F1F1F1"
+        />
+      </g>
+      <mask
+        id="mask2_1:52"
+        style={{ maskType: "alpha" as const }}
+        maskUnits="userSpaceOnUse"
+        x="1"
+        y="1"
+        width="71"
+        height="98"
+      >
+        <path
+          d="M45.398 1.43036H7.86688C4.22415 1.43036 1.24374 4.41077 1.24374 8.0535V91.9465C1.24374 95.5893 4.22415 98.5697 7.86688 98.5697H65.2674C68.9101 98.5697 71.8905 95.5893 71.8905 91.9465V27.9229L45.398 1.43036Z"
+          fill="white"
+        />
+      </mask>
+      <g mask="url(#mask2_1:52)">
+        <path
+          d="M47.3352 25.9856L71.8905 50.5354V27.9229L47.3352 25.9856Z"
+          fill="url(#paint0_linear_1:52)"
+        />
+      </g>
+      <mask
+        id="mask3_1:52"
+        style={{ maskType: "alpha" as const }}
+        maskUnits="userSpaceOnUse"
+        x="1"
+        y="1"
+        width="71"
+        height="98"
+      >
+        <path
+          d="M45.398 1.43036H7.86688C4.22415 1.43036 1.24374 4.41077 1.24374 8.0535V91.9465C1.24374 95.5893 4.22415 98.5697 7.86688 98.5697H65.2674C68.9101 98.5697 71.8905 95.5893 71.8905 91.9465V27.9229L45.398 1.43036Z"
+          fill="white"
+        />
+      </mask>
+      <g mask="url(#mask3_1:52)">
+        <path
+          d="M45.398 1.43036V21.2998C45.398 24.959 48.3618 27.9229 52.0211 27.9229H71.8905L45.398 1.43036Z"
+          fill="#87CEAC"
+        />
+      </g>
+      <mask
+        id="mask4_1:52"
+        style={{ maskType: "alpha" as const }}
+        maskUnits="userSpaceOnUse"
+        x="1"
+        y="1"
+        width="71"
+        height="98"
+      >
+        <path
+          d="M45.398 1.43036H7.86688C4.22415 1.43036 1.24374 4.41077 1.24374 8.0535V91.9465C1.24374 95.5893 4.22415 98.5697 7.86688 98.5697H65.2674C68.9101 98.5697 71.8905 95.5893 71.8905 91.9465V27.9229L45.398 1.43036Z"
+          fill="white"
+        />
+      </mask>
+      <g mask="url(#mask4_1:52)">
+        <path
+          d="M7.86688 1.43036C4.22415 1.43036 1.24374 4.41077 1.24374 8.0535V8.60542C1.24374 4.9627 4.22415 1.98229 7.86688 1.98229H45.398V1.43036H7.86688Z"
+          fill="white"
+          fillOpacity="0.2"
+        />
+      </g>
+      <mask
+        id="mask5_1:52"
+        style={{ maskType: "alpha" as const }}
+        maskUnits="userSpaceOnUse"
+        x="1"
+        y="1"
+        width="71"
+        height="98"
+      >
+        <path
+          d="M45.398 1.43036H7.86688C4.22415 1.43036 1.24374 4.41077 1.24374 8.0535V91.9465C1.24374 95.5893 4.22415 98.5697 7.86688 98.5697H65.2674C68.9101 98.5697 71.8905 95.5893 71.8905 91.9465V27.9229L45.398 1.43036Z"
+          fill="white"
+        />
+      </mask>
+      <g mask="url(#mask5_1:52)">
+        <path
+          d="M65.2674 98.0177H7.86688C4.22415 98.0177 1.24374 95.0373 1.24374 91.3946V91.9465C1.24374 95.5893 4.22415 98.5697 7.86688 98.5697H65.2674C68.9101 98.5697 71.8905 95.5893 71.8905 91.9465V91.3946C71.8905 95.0373 68.9101 98.0177 65.2674 98.0177Z"
+          fill="#263238"
+          fillOpacity="0.2"
+        />
+      </g>
+      <mask
+        id="mask6_1:52"
+        style={{ maskType: "alpha" as const }}
+        maskUnits="userSpaceOnUse"
+        x="1"
+        y="1"
+        width="71"
+        height="98"
+      >
+        <path
+          d="M45.398 1.43036H7.86688C4.22415 1.43036 1.24374 4.41077 1.24374 8.0535V91.9465C1.24374 95.5893 4.22415 98.5697 7.86688 98.5697H65.2674C68.9101 98.5697 71.8905 95.5893 71.8905 91.9465V27.9229L45.398 1.43036Z"
+          fill="white"
+        />
+      </mask>
+      <g mask="url(#mask6_1:52)">
+        <path
+          d="M52.0211 27.9229C48.3618 27.9229 45.398 24.959 45.398 21.2998V21.8517C45.398 25.511 48.3618 28.4748 52.0211 28.4748H71.8905V27.9229H52.0211Z"
+          fill="#263238"
+          fillOpacity="0.1"
+        />
+      </g>
+      <path
+        d="M45.398 1.43036H7.86688C4.22415 1.43036 1.24374 4.41077 1.24374 8.0535V91.9465C1.24374 95.5893 4.22415 98.5697 7.86688 98.5697H65.2674C68.9101 98.5697 71.8905 95.5893 71.8905 91.9465V27.9229L45.398 1.43036Z"
+        fill="url(#paint1_radial_1:52)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_1:52"
+          x1="59.6142"
+          y1="28.0935"
+          x2="59.6142"
+          y2="50.5388"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#263238" stopOpacity="0.2" />
+          <stop offset="1" stopColor="#263238" stopOpacity="0.02" />
+        </linearGradient>
+        <radialGradient
+          id="paint1_radial_1:52"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(3.48187 3.36121) scale(113.917)"
+        >
+          <stop stopColor="white" stopOpacity="0.1" />
+          <stop offset="1" stopColor="white" stopOpacity="0" />
+        </radialGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export const Logo = ({
+  className,
+  uniColor,
+}: {
+  className?: string;
+  uniColor?: boolean;
+}) => {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={cn("size-5", className)}
+    >
+      <path
+        d="M3 0H5V18H3V0ZM13 0H15V18H13V0ZM18 3V5H0V3H18ZM0 15V13H18V15H0Z"
+        fill={uniColor ? "currentColor" : "url(#logo-gradient)"}
+      />
+      <defs>
+        <linearGradient
+          id="logo-gradient"
+          x1="10"
+          y1="0"
+          x2="10"
+          y2="20"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#9B99FE" />
+          <stop offset="1" stopColor="#2BC8B7" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export const LogoStroke = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      className={cn("size-7 w-7", className)}
+      viewBox="0 0 71 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M61.25 1.625L70.75 1.5625C70.75 4.77083 70.25 7.79167 69.25 10.625C68.2917 13.4583 66.8958 15.9583 65.0625 18.125C63.2708 20.25 61.125 21.9375 58.625 23.1875C56.1667 24.3958 53.4583 25 50.5 25C46.875 25 43.6667 24.2708 40.875 22.8125C38.125 21.3542 35.125 19.2083 31.875 16.375C29.75 14.4167 27.7917 12.8958 26 11.8125C24.2083 10.7292 22.2708 10.1875 20.1875 10.1875C18.0625 10.1875 16.25 10.7083 14.75 11.75C13.25 12.75 12.0833 14.1875 11.25 16.0625C10.4583 17.9375 10.0625 20.1875 10.0625 22.8125L0 22.9375C0 19.6875 0.479167 16.6667 1.4375 13.875C2.4375 11.0833 3.83333 8.64583 5.625 6.5625C7.41667 4.47917 9.54167 2.875 12 1.75C14.5 0.583333 17.2292 0 20.1875 0C23.8542 0 27.1042 0.770833 29.9375 2.3125C32.8125 3.85417 35.7708 5.97917 38.8125 8.6875C41.1042 10.7708 43.1042 12.3333 44.8125 13.375C46.5625 14.375 48.4583 14.875 50.5 14.875C52.6667 14.875 54.5417 14.3125 56.125 13.1875C57.75 12.0625 59 10.5 59.875 8.5C60.7917 6.5 61.25 4.20833 61.25 1.625Z"
+        fill="none"
+        strokeWidth={0.5}
+        stroke="currentColor"
+      />
+    </svg>
+  );
+};
+
+// removed separate GoogleSheetsLogo export; Logo now renders the provided Google Sheets SVG
